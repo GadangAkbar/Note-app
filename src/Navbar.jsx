fa0aeb38da338da47be7844cf4e72be0af6adcb3
GadangAkbar <gadang227104@student.smkn1kandeman.sch.id>
2024-03-20 10:10:13 +0700

    fixing API

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -17,8 +17,10 @@ function Navbar({ token, onLogout }) {
                 )}
 
                 <nav className="flex gap-12">
-                    {token !== null ? null : <Link to={"/Regist"}><span className=" hover:text-sky-300 transition-all active:text-sky-300 focus:text-sky-300 ">Regist</span></Link>}
-                    {token !== null ? null : <Link to={"/Login"}><span className=" hover:text-sky-300 transition-all active:text-sky-300 focus:text-sky-300 ">Login</span></Link>}
+                    { <Link to={"/Regist"}><span className=" hover:text-sky-300 transition-all active:text-sky-300 focus:text-sky-300 ">Regist</span></Link>}
+                    { <Link to={"/Login"}><span className=" hover:text-sky-300 transition-all active:text-sky-300 focus:text-sky-300 ">Login</span></Link>}
+                    {/* {token !== null ? null : <Link to={"/Regist"}><span className=" hover:text-sky-300 transition-all active:text-sky-300 focus:text-sky-300 ">Regist</span></Link>}
+                    {token !== null ? null : <Link to={"/Login"}><span className=" hover:text-sky-300 transition-all active:text-sky-300 focus:text-sky-300 ">Login</span></Link>} */}
                     <Link to={"/Note"} className=" hover:text-sky-300 transition-all active:text-sky-300 focus:text-sky-300 ">Note</Link>
                     {/* {token !== null ? <Link onClick={() => onLogout()}><span className="text-white font-sans hover:text-slate-300">Logout</span></Link> : null} */}
                 </nav>
